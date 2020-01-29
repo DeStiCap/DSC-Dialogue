391e75ffdfc0ce90b1b346fc5f8328826dcae6d6
SupawichDechpuwarat <supawich.dechpuwarat@gmail.com>
2020-01-29 19:07:21 +0700

    V0.1.3-preview.6

--- Scripts/Controllers/DSC_Dialogue_TypingController.cs
+++ Scripts/Controllers/DSC_Dialogue_TypingController.cs
@@ -189,7 +189,13 @@ namespace DSC.Dialogue
             if (Time.time < m_fLastTypingTime + m_fTypingDelayTime)
                 return;
 
-            var hDialogueText = dialogueText;            
+            var hDialogueText = dialogueText;
+
+            if (m_hCurrentDialogue.m_sDialogue.Length <= m_nCurrentCharIndex)
+            {
+                EndTyping();
+                return;
+            }
             
             m_nCurrentCharIndex++;
 
@@ -203,12 +209,6 @@ namespace DSC.Dialogue
             }
 
             RunDialogueEventOnExecute(m_hCurrentDialogue);
-
-            if (hDialogueText.text.Length <= m_nCurrentCharIndex)
-            {
-                EndTyping();
-                return;
-            }
         }
 
         protected void EndTyping()
